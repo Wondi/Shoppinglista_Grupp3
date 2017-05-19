c555d4db96e678c66a7e5d742b70d6fd75d0c419
totor0 <daniel.issa90@gmail.com>
2017-05-19 10:50:10 +0200

    Deleting shopping list in progress

---
+++ features/step-definitions/delete-shopping-list.js
@@ -0,0 +1,36 @@
+let assert = require('assert');
+let {defineSupportCode} = require('cucumber');
+let GroceryList = require('../../grocery-list.js');
+let GroceryListItem = require('../../grocery-list-item.js');
+let AppGroceryLists = require('../../app-grocery-lists.js');
+
+defineSupportCode(function({Given, When, Then}) {
+
+	let theList, runtimeErrorOnNolist, groceryList
+
+ 	 Given('that I have a saved shopping list', function (callback) {
+         theList = new AppGroceryLists('Fredagsmys');
+         theList.
+         callback();
+
+       });
+
+ 		When('I delete the shopping list from database', function (callback) {
+         
+         callback();
+       
+       });
+
+ 		 Then('that particular shopping list should be deleted from the database', function (callback) {
+         
+         callback();
+       
+       });
+
+ 		 Then('I should be able to get a confirmation message about it.', function (callback) {
+         // Write code here that turns the phrase above into concrete actions
+         
+         callback();
+       
+       });
+}
