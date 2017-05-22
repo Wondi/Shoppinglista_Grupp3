69e0a549662f062f852e408a199acb843d65714c
vivipapa <papaparaskevi@gmail.com>
2017-05-22 14:09:28 +0200

    Changes to sort by name

--- features/step-definitions/sort-items-by-name.js
+++ features/step-definitions/sort-items-by-name.js
@@ -10,10 +10,10 @@ defineSupportCode(function({Given, When, Then}) {
     Given('that I have a shopping list with named items', function (callback) {
      // Write code here that turns the phrase above into concrete actions
      groceryList = new GroceryList('Torsdag');
-     groceryList.addToList('Banan',10,"frukt");
-     groceryList.addToList('Fläsksvål',10,"kött");
-     groceryList.addToList('Äpple',10,"frukt");
-     groceryList.addToList('Oxsvans',10,"kött");
+     groceryList.addToList('Banan',5,"frukt");
+     groceryList.addToList('Fläsksvål',3,"kött");
+     groceryList.addToList('Persika',10,"frukt");
+     groceryList.addToList('Oxsvans',1,"kött");
      callback();
     });
 
@@ -26,19 +26,12 @@ defineSupportCode(function({Given, When, Then}) {
     Then('I should be able to get a list with the items sorted by their name.', function (callback) {
      // Write code here that turns the phrase above into concrete actions
          assert(sortedList.length === groceryList.items.length);
-         //Generic solution for long lists
-         let foundNames = [];
-         for(item of sortedList){
-            if(foundNames.indexOf(item.name)<0){
-                // first time we see this name
-                foundNames.push(item.name);
-            }
-            else if(foundNames[foundNames.length-1] !== item.name){
-                // not sorted since a name repeats after we have
-                // seen another one
-                assert(false);
-            }
-         }
+
+         assert(sortedList[0].name === 'Banan');
+         assert(sortedList[1].name === 'Fläsksvål');
+         assert(sortedList[2].name === 'Oxsvans');
+         assert(sortedList[3].name === 'Persika');
+         
          callback();
     });
 });
