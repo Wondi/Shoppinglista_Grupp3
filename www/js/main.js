6cd3dbabd903f54af5806bda65489542a4f6c8c0
vivipapa <papaparaskevi@gmail.com>
2017-05-25 20:00:39 +0200

    Trying to fix the functions from WR4

--- www/js/main.js
+++ www/js/main.js
@@ -5,6 +5,8 @@ let l = new GroceryList("Hepp");
 l.addToList("Bananer",2,"frukt");
 l.addToList("Äpplen",3,"frukt");
 l.addToList("Morötter",10,"grönsaker");
+l.addToList("File", 5, "Kött");
+l.buy("Äpplen");
 //wait for DOM
 $(function(){
   l.showAllItems();
