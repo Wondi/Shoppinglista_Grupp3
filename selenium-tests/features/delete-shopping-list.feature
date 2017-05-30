e6eec86c435195f2183d6a2f726a55d89cf1a389
vivipapa <papaparaskevi@gmail.com>
2017-05-30 22:35:46 +0200

    Delete shopping list. Test done and running.

--- selenium-tests/features/delete-shopping-list.feature
+++ selenium-tests/features/delete-shopping-list.feature
@@ -3,8 +3,8 @@ Feature: Delete a shopping list.
 			I should be able to delete my saved shopping list
 			So that I can see just the ones that I want to keep.
 
-	Scenario: As a user I should be able to delete a saved shopping list.
-			  Given that I have a saved shopping list
-			  When I delete the shopping list from list of all shopping-lists
-			  Then that particular shopping list should be deleted from list of all shopping-lists.
+	Scenario: As a user I should be able to delete a shopping list from my collection.
+			  Given that I have a shopping list in my collection of lists
+			  When I try to delete the shopping list from my collection of lists
+			  Then that particular shopping list should be deleted from my collection of lists.
 			
--- selenium-tests/step-definitions/delete-shopping-list.js
+++ selenium-tests/step-definitions/delete-shopping-list.js
@@ -1,17 +1,35 @@
 module.exports = function () {
-	this.Given(/^that I have a saved shopping list$/, async function() {
+
+	this.Given(/^that I have a shopping list in my collection of lists$/, async function() {
 	  	await helpers.loadPage("http://localhost:3000/#create_new_shopping_list");
-  		await driver.findElement(by.css("#list_name")).sendKeys("My list");
+  		await driver.findElement(by.css("#list_name")).sendKeys("Måndag");
 		await driver.findElement(by.css(".createBtn")).click();
+		await driver.findElement(By.linkText("New shopping list")).click();
+		await driver.findElement(by.css("#list_name")).sendKeys("Tisdag");
+		await driver.findElement(by.css(".createBtn")).click();
+		await driver.findElement(By.linkText("New shopping list")).click();
+		await driver.findElement(by.css("#list_name")).sendKeys("Onsdag");
+		await driver.findElement(by.css(".createBtn")).click();
+		await driver.sleep(2000);
 		await driver.findElement(By.linkText("All lists")).click();
 		await driver.sleep(3000);
 	});
 
-	this.When(/^I delete the shopping list from list of all shopping-lists$/, async function() {
-		await driver.findElement(by.css(".deleteList")).click();
+	this.When(/^I try to delete the shopping list from my collection of lists$/, async function() {
+		await driver.findElement(by.css('#all_list tr:nth-child(2) .deleteList')).click();
+		await driver.sleep(3000);
 	});
 
-	this.Then(/^that particular shopping list should be deleted from list of all shopping-lists.$/, async function() {
-	
+	this.Then(/^that particular shopping list should be deleted from my collection of lists.$/, async function() {
+		let trs = await driver.findElements(by.css('#all_list table tr'));
+		let nameTds = await driver.findElements(by.css('#all_list table tr td:nth-child(2)'));
+        console.log("trs", trs.length);
+        console.log("nameTds", nameTds.length);
+        for(let td of nameTds){
+  			let text = await td.getText();
+  			console.log("text: ",text);
+  			assert(text != "Tisdag", "List was not removed");
+  		}
+  		assert(trs.length===3,"Not removed");
 	});
 }
